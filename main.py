222a78d7a57a7716433c474fa304604c882227d2
Altaryu <jeffgar4@gmail.com>
2026-03-17 19:13:20 -0400

    Began working on ability to accept multiple messages.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import argparse
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key=os.environ.get("GEMINI_API_KEY")
@@ -13,10 +14,12 @@ parser=argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
 args=parser.parse_args()
 
+messages=[types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
 client=genai.Client(api_key=api_key) 
 response=client.models.generate_content(
     model="gemini-2.5-flash",
-contents=args.user_prompt
+contents=messages
 )
 if response.usage_metadata != None:
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
