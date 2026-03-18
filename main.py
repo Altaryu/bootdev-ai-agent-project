ddd361bb28f8b453fbb51ec389f9f83197c58baf
Altaryu <jeffgar4@gmail.com>
2026-03-18 16:55:34 -0400

    Added verbose response.

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ if api_key == None:
 
 parser=argparse.ArgumentParser(description="Chatbot")
 parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
 args=parser.parse_args()
 
 messages=[types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
@@ -21,9 +22,12 @@ response=client.models.generate_content(
     model="gemini-2.5-flash",
 contents=messages
 )
-if response.usage_metadata != None:
+if response.usage_metadata != None and args.verbose:
+    print(f"User prompt: {args.user_prompt}")
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    print(f"Response: {response.text}")
+elif response.usage_metadata != None and args.verbose == False:
+    print(response.text)
 else:
     raise RuntimeError("no usage detected")
-print(response.text)
