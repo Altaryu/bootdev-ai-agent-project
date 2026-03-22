40ea4d24873bf49e939d4ff4a55ed03a0f2ed65f
Altaryu <jeffgar4@gmail.com>
2026-03-22 17:24:54 -0400

    Gave LLM the ability to call functions

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import os
 import argparse
 from prompt import system_prompt
-from call_function import available_functions
+from call_function import available_functions, call_function
 from dotenv import load_dotenv
 from google import genai
 from google.genai import types
@@ -19,6 +19,7 @@ args=parser.parse_args()
 
 model_name="gemini-2.5-flash"
 messages=[types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+function_results=[]
 
 client=genai.Client(api_key=api_key) 
 response=client.models.generate_content(
@@ -37,6 +38,18 @@ if response.usage_metadata != None and args.verbose:
     if response.function_calls != None:
         for function in response.function_calls:
             print(f"Calling function: {function.name} ({function.args})")
+            call_function_result=call_function(function, args.verbose)
+            if call_function_result.parts == []:
+                raise Exception("parts should not be empty")
+            else:
+                if call_function_result.parts[0].function_response == None:
+                    raise Exception("parts[0] should not be None")
+                elif call_function_result.parts[0].function_response.response == None:
+                    raise Exception("response should not be None")
+                else:
+                    function_results.append(call_function_result.parts[0])
+                    print(f"-> {call_function_result.parts[0].function_response.response}")
+    
 
 
 elif response.usage_metadata != None and args.verbose == False:
@@ -44,5 +57,16 @@ elif response.usage_metadata != None and args.verbose == False:
     if response.function_calls != None:
         for function in response.function_calls:
             print(f"Calling function: {function.name} ({function.args})")
+            #call_function_result=call_function(function, args.verbose)
+            #if call_function_result.types.Content.parts == []:
+            #    raise Exception("parts should not be empty")
+            #else:
+            #    if call_function_result.parts[0].function_response == None:
+            #        raise Exception("parts[0] should not be None")
+            #    elif call_function_result.parts[0].function_response.response == None:
+            #        raise Exception("response should not be None")
+            #    else:
+            #        function_results.append(call_function_result.parts[0])
+            #        print(f"-> {call_function_result.parts[0].function_response.response}")
 else:
     raise RuntimeError("no usage detected")
